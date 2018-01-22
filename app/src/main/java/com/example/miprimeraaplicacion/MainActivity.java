2f08cf75c7727dbd04b410dd1aec6c18e8ad2548
VicenteFnowell <35596122+VicenteFnowell@users.noreply.github.com>
2018-01-22 10:38:51 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int edad = 3;
+        int edad = 4;
         String nombre = "Juan";
         boolean flag = "true";
 
